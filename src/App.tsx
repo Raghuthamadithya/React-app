4026e29cc7621cb22d98f58fb0409d605f29c0d1
Raghuthamadithya <acharya.mudumbai@zemosolabs.com>
2022-05-09 10:51:50 +0530

    commit for react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+
+import MyLibraryTitle from './Atoms/MylibraryTitle/MyLibraryTitle';
+import MyTabs from './Atoms/Tabs/MyTabs';
+import MyAppBar from './Organisms/AppBar/MyAppBar';
+import AppBarWithCategories from './Organisms/AppBarWithCategories/AppBarWithCategories';
+import { Footer } from './Organisms/Footer/Footer';
+
+
 
 function App() {
+
+   const [value,setValue]=useState(0);
+   const [searchTerm,setSearchTerm]=useState('');
+
+   const handleChange=(e:React.MouseEventHandler<SVGSVGElement>,newValue: number)=>{
+    setValue(newValue);
+    
+  }
+  const handleSearchTerm= (e:React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>,newValue:string)=>
+  {
+     setSearchTerm(newValue);
+  }
+
+  const closeSearch = ()=>
+  {
+    setSearchTerm('');
+  }
+
+  
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+<>     
+    <div id="appbar">
+       
+    { value===1 &&  <AppBarWithCategories handleChange={handleChange}/> }
+    {value===0 && <MyAppBar handleChange={handleChange}
+    handleSearchTerm={handleSearchTerm}  closeSearch={closeSearch}/>  }
+
+  <div id="librarytitle" style={{paddingLeft: '264px',marginTop:'89px'}}>
+    <MyLibraryTitle />
     </div>
+
+    <div id="mytabs" style={{paddingLeft: '264px'}}>
+    <MyTabs searchTerm={searchTerm} />
+    </div>
+
+       </div>    
+
+    <Footer />
+
+
+    </>
+
+   
   );
 }
+
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
+
+import './index.css';
+import Page2 from './Page2';
+import Page3 from './Page3';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+
+ReactDOM.render(
+    
+ 
+      <Router >
+      <Routes>
+          <Route path="/explore" element={<Page2 />} />
+          <Route path="/" element={<App />} />
+          <Route path="/bookdetail" element={<Page3 />} />
+      </Routes>
+    </Router> 
+   
+     
+   ,
+    document.getElementById('root')
+  );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
